fb118caa1e17e073e58f560c501572885596f8b5
cabrah13 <charlottemabraham32@gmail.com>
2026-03-02 15:24:53 -0500

    add way for cards to appear in HTML doc

--- script.js
+++ script.js
@@ -3,6 +3,10 @@
 const addBtn = document.getElementById('addNoteBtn');
 const popUp = document.getElementById('notePopUp');
 const closeBtn = document.getElementById('closeBtn');
+const submitNoteBtn = document.getElementById('submitNoteBtn');
+const noteTitle = document.getElementById('noteTitle');
+const noteContent = document.getElementById('noteContent');
+const notesContainer = document.getElementById('notesContainer');
 
 let notes = [];
 
@@ -17,7 +21,26 @@ closeBtn.addEventListener('click', () => {
 //CRUD Operations 
 
 //Create (Person A)
+submitNoteBtn.addEventListener('click', () => {
+    const titleInput = noteTitle.value; 
+    const contentInput = noteContent.value;
 
+    if(titleInput !== "" && contentInput !== "") {
+        notes.unshift({noteTitle: titleInput.trim(), noteContent: contentInput.trim()});
+        
+        const cardItem = `<div class = "card">
+            <h3>${titleInput.trim()}</h3>
+            <p>${contentInput.trim()}</p>
+        </div>`
+
+        notesContainer.innerHTML += cardItem;
+        noteTitle.value = '';
+        noteContent.value = '';
+        
+    }
+
+    popUp.close();
+});
 //Read (Person A)
 
 //Update/Edit (Person B)
