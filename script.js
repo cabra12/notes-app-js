8a84a6b32494a8d7f0cd894560e30d037f279bc2
cabrah13 <charlottemabraham32@gmail.com>
2026-03-21 11:28:34 -0400

    change styles and simplify JS code

--- script.js
+++ script.js
@@ -8,30 +8,41 @@ const noteTitle = document.getElementById('noteTitle');
 const noteContent = document.getElementById('noteContent');
 const notesContainer = document.getElementById('notesContainer');
 
+
 let notes = [];
 
-addBtn.addEventListener('click', ()=> {
-    popUp.showModal();
-});
+const onLoad = () => {
+    if(notes.length === 0) {
+        const divCard = document.createElement('div');
+        divCard.classList.add('note-card');
+        const emptyCardText = document.createTextNode("Click 'Add Note' to start adding cards!");
+        const plusSign = document.createTextNode('+');
+        // plusSign.style.fontSize = '2rem';
+        notesContainer.appendChild(divCard);
+        divCard.appendChild(emptyCardText);
+        divCard.style.display = 'block';
+        // emptyCardText.appendChild(plusSign);
+    }else {
+        divCard.style.display = 'none';
+    }
+};
 
-closeBtn.addEventListener('click', () => {
-    popUp.close();
-});
 
 //CRUD Operations 
 
 //Create (Person A)
-submitNoteBtn.addEventListener('click', () => {
+
+const createNote = () => {
     const titleInput = noteTitle.value; 
     const contentInput = noteContent.value;
 
     if(titleInput !== "" && contentInput !== "") {
         notes.unshift({noteTitle: titleInput.trim(), noteContent: contentInput.trim()});
-        
+
         //converts items in array for HTML rendering, using map to simplify 
         //.join() to make array that map produces into a string for innerHTML
         const cardItem = notes.map(item => {
-        return `<div class = "card">
+        return `<div class="note-card">
             <h3>${item.noteTitle}</h3>
             <p>${item.noteContent}</p>
         </div>`;
@@ -44,10 +55,13 @@ submitNoteBtn.addEventListener('click', () => {
     }
 
     popUp.close();
-});
-//Read (Person A)
+};
 
-//Update/Edit (Person B)
 
-//Delete (Person B)
+//Event Listener
+
+addBtn.addEventListener('click', () => popUp.showModal());
+closeBtn.addEventListener('click', () => popUp.close());
+document.addEventListener('DOMContentLoaded', onLoad);
+submitNoteBtn.addEventListener('click', createNote);
 
