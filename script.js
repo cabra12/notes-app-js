4c503c1703d7783711f09fc53bd7341275d2f396
cabrah13 <charlottemabraham32@gmail.com>
2026-03-22 10:06:33 -0400

    add unique IDs to notes for later deletion

--- script.js
+++ script.js
@@ -8,12 +8,14 @@ const noteTitle = document.getElementById('noteTitle');
 const noteContent = document.getElementById('noteContent');
 const notesContainer = document.getElementById('notesContainer');
 
+const deleteBtn = document.getElementById('delete');
+
 
 let notes = [];
 
 const onLoad = () => {
     if(notes.length === 0) {
-        const divCard = document.createElement('div');
+        const divCard = document.createElement('h3');
         divCard.classList.add('note-card');
         const emptyCardText = document.createTextNode("Click 'Add Note' to start adding cards!");
         const plusSign = document.createTextNode('+');
@@ -30,32 +32,39 @@ const onLoad = () => {
 
 //CRUD Operations 
 
-//Create (Person A)
+//Create and Read
 
-const createNote = () => {
+const createNote = (e) => {
+    e.preventDefault();
     const titleInput = noteTitle.value; 
     const contentInput = noteContent.value;
-
-    if(titleInput !== "" && contentInput !== "") {
-        notes.unshift({noteTitle: titleInput.trim(), noteContent: contentInput.trim()});
-
-        //converts items in array for HTML rendering, using map to simplify 
-        //.join() to make array that map produces into a string for innerHTML
-        const cardItem = notes.map(item => {
-        return `<div class="note-card">
-            <h3>${item.noteTitle}</h3>
-            <p>${item.noteContent}</p>
-        </div>`;
-        }).join('');
-
-        notesContainer.innerHTML = cardItem;
-        noteTitle.value = '';
-        noteContent.value = '';
         
-    }
+    notes.unshift({noteID: Date.now().toString(36), noteTitle: titleInput.trim(), noteContent: contentInput.trim()});
+
+    //converts items in array for HTML rendering, using map to simplify 
+    //.join() to make array that map produces into a string for innerHTML
+    const cardItem = notes.map(item => {
+    return `<div class="note-card">
+        <div class="card-buttons">
+            <p>id:${item.noteID}</p>
+            <button id="edit">Edit</button>
+            <button id="delete">Delete</button>
+        </div>
+        <h3>${item.noteTitle}</h3>
+        <p>${item.noteContent}</p>
+    </div>`;
+    }).join('');
+
+    notesContainer.innerHTML = cardItem;
+    noteTitle.value = '';
+    noteContent.value = '';
 
     popUp.close();
 };
+
+const deleteNote = (item) => {
+    console.log(item.noteID);
+}
 
 
 //Event Listener
@@ -64,4 +73,5 @@ addBtn.addEventListener('click', () => popUp.showModal());
 closeBtn.addEventListener('click', () => popUp.close());
 document.addEventListener('DOMContentLoaded', onLoad);
 submitNoteBtn.addEventListener('click', createNote);
+deleteBtn.addEventListener('click', deleteNote);
 
