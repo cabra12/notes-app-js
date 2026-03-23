b8aa322d4206d784ad25b1c10aad6e99b74b397a
cabrah13 <charlottemabraham32@gmail.com>
2026-03-23 11:10:19 -0400

    add localStorage and delete functionality

--- script.js
+++ script.js
@@ -8,70 +8,71 @@ const noteTitle = document.getElementById('noteTitle');
 const noteContent = document.getElementById('noteContent');
 const notesContainer = document.getElementById('notesContainer');
 
-const deleteBtn = document.getElementById('delete');
-
-
-let notes = [];
+let notes = JSON.parse(localStorage.getItem('notes')) || [];
 
 const onLoad = () => {
+    const divCard = document.createElement('h3');
+    divCard.classList.add('note-card');
+    const emptyCardText = document.createTextNode("Click 'Add Note' to start adding cards!");
+    notesContainer.appendChild(divCard);
+    divCard.appendChild(emptyCardText);
+
     if(notes.length === 0) {
-        const divCard = document.createElement('h3');
-        divCard.classList.add('note-card');
-        const emptyCardText = document.createTextNode("Click 'Add Note' to start adding cards!");
-        const plusSign = document.createTextNode('+');
-        // plusSign.style.fontSize = '2rem';
-        notesContainer.appendChild(divCard);
-        divCard.appendChild(emptyCardText);
         divCard.style.display = 'block';
-        // emptyCardText.appendChild(plusSign);
     }else {
         divCard.style.display = 'none';
+        createNote(notes);
     }
 };
 
-
-//CRUD Operations 
-
-//Create and Read
-
-const createNote = (e) => {
+const submitNote = (e) => {
     e.preventDefault();
     const titleInput = noteTitle.value; 
     const contentInput = noteContent.value;
         
     notes.unshift({noteID: Date.now().toString(36), noteTitle: titleInput.trim(), noteContent: contentInput.trim()});
 
-    //converts items in array for HTML rendering, using map to simplify 
-    //.join() to make array that map produces into a string for innerHTML
-    const cardItem = notes.map(item => {
-    return `<div class="note-card">
-        <div class="card-buttons">
-            <p>id:${item.noteID}</p>
-            <button id="edit">Edit</button>
-            <button id="delete">Delete</button>
-        </div>
-        <h3>${item.noteTitle}</h3>
-        <p>${item.noteContent}</p>
-    </div>`;
-    }).join('');
+    createNote(notes);
 
-    notesContainer.innerHTML = cardItem;
+    localStorage.setItem('notes', JSON.stringify(notes));
     noteTitle.value = '';
     noteContent.value = '';
 
     popUp.close();
 };
 
-const deleteNote = (item) => {
-    console.log(item.noteID);
+const createNote = (notes) => {
+    //converts items in array for HTML rendering, using map to simplify 
+    //.join() to make array that map produces into a string for innerHTML
+    const cardItem = notes.map(item => {
+        return `<div class="note-card" data-id="${item.noteID}">
+            <div class="card-buttons">
+                <button class="edit">Edit</button>
+                <button class="delete">Delete</button>
+            </div>
+            <h3>${item.noteTitle}</h3>
+            <p>${item.noteContent.length > 100 ? item.noteContent.slice(0, 100) + "..." : item.noteContent}</p>
+        </div>`;
+    }).join('');
+
+    notesContainer.innerHTML = cardItem;
 }
 
+const deleteNote = (e) => {
+    if(e.target.classList.contains('delete')) {
+        const id = e.target.closest('.note-card').dataset.id;
+        notes = notes.filter((note) => note.noteID !== id);
+        localStorage.setItem('notes', JSON.stringify(notes));
+        createNote(notes);
+        onLoad();
+    }
+}
 
 //Event Listener
 
 addBtn.addEventListener('click', () => popUp.showModal());
 closeBtn.addEventListener('click', () => popUp.close());
 document.addEventListener('DOMContentLoaded', onLoad);
-submitNoteBtn.addEventListener('click', createNote);
-deleteBtn.addEventListener('click', deleteNote);
+submitNoteBtn.addEventListener('click', submitNote);
+notesContainer.addEventListener('click', deleteNote);
 
